d38e4f139a1a26165c00a993526817c8d1676f0f
Alvaro <amarflo1406@g.educaand.es>
2024-04-04 16:51:40 +0200

    intenti de api

---
+++ application/controllers/RestServer.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+require APPPATH . '/libraries/RestController.php';
+require APPPATH . '/libraries/Format.php';
+
+
+
+class RestServer extends RestController
+{
+	// Metodo get
+	public function  test_get(){
+		$uwu = array("a","b","c");
+		$this->response($uwu);
+	}
+
+}
